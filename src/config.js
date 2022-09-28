cb4f4f370604c081e354ec803745ef81666ca7cd
JakubTenZajebistyProgramista <104985908+GildiaCTP@users.noreply.github.com>
2022-09-28 17:28:07 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     clientConfig: {
-        clientToken: "MTAyMzE2MDI3NTAxODQ2NTI4MA.GGg9-P._yVbId_QB5k0OSxay7J1GGcARwuPhws77XCUiw", //token bota
-        clientLogs: "1022933069990203539", //id kanału z logami
-        guildId: "1022933067079372850",
+        clientToken: "MTAyMzE2MDI3NTAxODQ2NTI4MA.G7M9hY.JN_eECxkY0QWeuhfaIHCOk6QQvNu95lywpa4BI", //token bota
+        clientLogs: "1022933067079372850", //id kanału z logami
+        guildId: "1022933069990203539",
     },
 };
